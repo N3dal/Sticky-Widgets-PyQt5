d53162f1559f3be875de03620087292afca57600
N3dal <nedalxzo@gmail.com>
2024-03-04 23:22:19 +0200

    added movement functions

--- main.py
+++ main.py
@@ -19,6 +19,46 @@ from sys import (argv, exit)
 from exceptions import *
 
 
+class StickyWidget(QFrame):
+
+    WIDTH, HEIGHT = (180, 180)
+    STYLESHEET = """
+        background-color: #47D7D1;
+        border-radius: 5px;
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setFixedSize(StickyWidget.WIDTH, StickyWidget.HEIGHT)
+        self.setStyleSheet(StickyWidget.STYLESHEET)
+
+        # intial_x, intial_y = self.x(), self.y()
+        self.__mouse_x, self.__mouse_y = 0, 0
+
+        self.__enable_movement = False
+
+    def mouseDoubleClickEvent(self, e):
+        self.__enable_movement = True
+
+        self.__mouse_x, self.__mouse_y = e.x(), e.y()
+
+    def mouseMoveEvent(self, e):
+
+        if not self.__enable_movement:
+            return
+
+        dx = e.windowPos().x() - self.__mouse_x
+        dy = e.windowPos().y() - self.__mouse_y
+
+        self.move(int(dx), int(dy))
+
+    def mouseReleaseEvent(self, e):
+
+        if self.__enable_movement:
+            self.__enable_movement = False
+
+
 class MainWindow(QMainWindow):
 
     WIDTH, HEIGHT = (800, 600)
@@ -40,6 +80,14 @@ class MainWindow(QMainWindow):
 
         self.setFixedSize(MainWindow.WIDTH, MainWindow.HEIGHT)
         self.setStyleSheet(MainWindow.STYLESHEET)
+
+        self.__setup_widgets()
+
+    def __setup_widgets(self):
+
+        frame = StickyWidget(parent=self)
+        frame.move(100, 200)
+        frame.show()
 
 
 def main():
